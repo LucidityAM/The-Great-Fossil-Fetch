a4f032f4feadc80557b639281688270dfb5b07ea
LucidityAM <LucidityAM@gmail.com>
2020-05-21 12:46:14 -0700

    Music
    Added Music code

---
+++ Assets/Tristan Code/Music/Scripts/MusicManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MusicManager 
+{
+    public static bool overWorldMusic;
+    public static bool normalBattleMusic;
+    public static bool bossBattleMusic;
+}
---
+++ Assets/Tristan Code/Music/Scripts/MusicPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource overWorld;
+    public AudioSource normalBattle;
+    public AudioSource bossBattle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //reseting all bools
+        MusicManager.overWorldMusic = false;
+        MusicManager.normalBattleMusic = false;
+        MusicManager.bossBattleMusic = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks what bool is true, and plays audio accordingly
+        if (MusicManager.normalBattleMusic == true)
+        {
+            overWorld.time = 0f;
+            bossBattle.time = 0f;
+            normalBattle.UnPause();
+            overWorld.Pause();
+            bossBattle.Pause();
+
+        } else if (MusicManager.bossBattleMusic == true)
+        {
+            normalBattle.time = 0f;
+            overWorld.time = 0f;
+            bossBattle.UnPause();
+            overWorld.Pause();
+            normalBattle.Pause();
+        }
+        else
+        {
+            normalBattle.time = 0f;
+            bossBattle.time = 0f;
+            overWorld.UnPause();
+            normalBattle.Pause();
+            bossBattle.Pause();
+        }
+    }
+}

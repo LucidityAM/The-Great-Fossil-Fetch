0029c16e0edfee6028d65b5439ad2fad0826077b
LucidityAM <LucidityAM@gmail.com>
2020-05-31 19:30:27 -0700

    added win screen and all animations for it

--- Assets/Isaiah Code/Sprites/Battle UI/Win Screen/WinScreenAnimation.cs
+++ Assets/Isaiah Code/Sprites/Battle UI/Win Screen/WinScreenAnimation.cs
@@ -15,6 +15,10 @@ public class WinScreenAnimation : MonoBehaviour
     void Start()
     {
         WinScreen.SetActive(false);
+
+        fossilsAnim.SetBool("isOpen", false);
+        artAnim.SetBool("isOpen", false);
+        HPAnim.SetBool("isOpen", false);
     }
 
     // Update is called once per frame
@@ -26,6 +30,27 @@ public class WinScreenAnimation : MonoBehaviour
     public IEnumerator OpenWinScreen()
     {
         WinScreen.SetActive(true);
+        artAnim.SetBool("isOpen", true);
+        yield return new WaitForSeconds(0.3f);
+        fossilsAnim.SetBool("isOpen", true);
+        yield return new WaitForSeconds(0.4f);
+        HPAnim.SetBool("isOpen", true);
+    }
+
+    public void StartIEnumerator(string name)
+    {
+        StartCoroutine(name);
+    }
+
+    public IEnumerator CloseWinScreen()
+    {
+        HPAnim.SetBool("isOpen", false);
+        yield return new WaitForSeconds(0.3f);
+        artAnim.SetBool("isOpen", false);
+        yield return new WaitForSeconds(0.4f);
+        fossilsAnim.SetBool("isOpen", false);
+        yield return new WaitForSeconds(0.6f);
+        WinScreen.SetActive(false);
 
     }
 }

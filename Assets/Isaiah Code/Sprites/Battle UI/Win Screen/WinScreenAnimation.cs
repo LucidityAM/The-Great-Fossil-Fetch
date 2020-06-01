c8e37becbb1970005d6d5cf993239a4e1fa17afc
LucidityAM <LucidityAM@gmail.com>
2020-05-31 19:12:38 -0700

    Created WiNScreen prefab and anims for win screen

---
+++ Assets/Isaiah Code/Sprites/Battle UI/Win Screen/WinScreenAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreenAnimation : MonoBehaviour
+{
+    //takes whole game screen Object
+    public GameObject WinScreen;
+
+    public Animator fossilsAnim;
+    public Animator artAnim;
+    public Animator HPAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WinScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator OpenWinScreen()
+    {
+        WinScreen.SetActive(true);
+
+    }
+}
